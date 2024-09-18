49aed49e9302554fca57e3ed867424b0ad882f7b
ASUS TUF Gaming F15 <Spartan.0637@outlook.com>
2024-09-18 09:17:26 -0600

    [Add] - BLL CRUD completo Camiones

--- BLL/BLL_Camiones.cs
+++ BLL/BLL_Camiones.cs
@@ -11,9 +11,21 @@ namespace BLL
     public class BLL_Camiones
     {
         //CRUD
+        public static string insertar_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.insertar_Camion(camion);
+        }
         public static List<Camiones_VO> get_Camiones(params object[] parametros)
         {
             return DAL_Camiones.get_Camiones(parametros);
+        }
+        public static string update_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.update_Camion(camion);
+        }
+        public static string eliminar_Camion(int id)
+        {
+            return DAL_Camiones.eliminar_Camion(id);
         }
     }
 }
