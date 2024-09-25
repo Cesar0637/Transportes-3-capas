c1f1e983743d61a8a625802d517f24ad9d0446b5
ASUS TUF Gaming F15 <Spartan.0637@outlook.com>
2024-09-25 13:28:39 -0600

    [Add] - Rutas[EF, Linq] + RutasEF (flujo completo con ef automatico)

---
+++ DTO/Viajes_DetallesDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Viajes_DetallesDTO
+    {
+        public int ID_Cargamento { get; set; }
+        public string Cargamento { get; set; }
+        public int ID_Direccion_Origen { get; set; }
+        public string Origen { get; set; }
+        public string Estado_Origen { get; set; }
+        public int ID_Direccion_Destino { get; set; }
+        public string Destino { get; set; }
+        public string Estado_Destino { get; set; }
+        public int Id_Chofer { get; set; }
+        public string Chofer { get; set; }
+        public int Id_Camion { get; set; }
+        public string Camión { get; set; }
+        public DateTime Salida { get; set; }
+        public DateTime Llegada_Estimada { get; set; }
+    }
+}
