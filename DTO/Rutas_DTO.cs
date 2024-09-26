9e30a49876f8250bafef4a3a1672eb49955d02af
ASUS TUF Gaming F15 <Spartan.0637@outlook.com>
2024-09-26 11:41:13 -0600

    [Add] WCF Calc Y rutas

---
+++ DTO/Rutas_DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Rutas_DTO
+    {
+        public int Id_Ruta { get; set; }
+        public double distancia { get; set; }
+        public DateTime fecha_salida { get; set; }
+        public DateTime fecha_llegada_estimada { get; set; }
+        public int Camion_id { get; set; }
+        public int Chofer_id { get; set; }
+        public int Direccion_Origen_Id { get; set; }
+        public int Direccion_Destino_Id { get; set; }
+        public int Cargamento_Id { get; set; }
+    }
+}
